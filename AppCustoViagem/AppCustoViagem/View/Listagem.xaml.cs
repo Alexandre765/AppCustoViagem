95ab7fd8fde31b14fc75eb4852ee989b62d3cf07
Usuario <Usuario@CPD03-M09>
2022-09-13 12:19:38 -0300

    Adicionar arquivos de projeto.

---
+++ AppCustoViagem/AppCustoViagem/View/Listagem.xaml.cs
@@ -0,0 +1,54 @@
+﻿using AppCustoViagem.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppCustoViagem.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Listagem : ContentPage
+    {
+        public Listagem()
+        {
+            InitializeComponent();
+        }
+
+        private void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Formulario());
+        }
+
+        protected override void OnAppearing()
+        {
+            try
+            {
+                lst_pedagios.ItemsSource = App.ListaPedagios;
+            } catch (Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "Ok");
+            }
+        }
+
+        private async void MenuItem_Clicked(object sender, EventArgs e)
+        {
+            MenuItem disparador = (MenuItem)sender;
+
+            Pedagio pedagio_selecionado = (Pedagio)disparador.BindingContext;
+
+            bool confirmacao = await DisplayAlert("Tem Certeza?",
+                                                  "Excluir pedágio " + pedagio_selecionado.Localização + "?",
+                                                  "Sim", "Não");
+            if (confirmacao)
+            {
+                App.ListaPedagios.Remove(pedagio_selecionado);
+
+                lst_pedagios.ItemsSource = App.ListaPedagios;
+            }
+        }
+    }
+}
